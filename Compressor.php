10a9e3c712b7ae376eddbbb28078c81865d3d0b2
Siim Raudsepp <Siim.Raudsepp@tptlive.ee>
2017-10-31 13:15:08 +0200

    Parandus arrayga

--- Compressor.php
+++ Compressor.php
@@ -3,6 +3,8 @@
 	$input = "000001110101101";
 	$output = compress($input);
 	echo $output;
+	$normal = decompresser($output);
+	echo $normal;
 	
 	function compress($argument)
 	{
@@ -10,7 +12,7 @@
 		//for annab array sümboli, see lisatakse ajutisse stringi, mis lisatakse lõpp stringi.
 		//if laused kontrollivad millal temp fail nullitakse,
 		//strval muudab countsyb'i stringiks
-		$array = explode("",$argument);
+		$array = array($argument);
 		$tempsymb = "";
 		$countsyb = 0;
 		for($i = 0;$i > Count($array);$i++)
@@ -26,11 +28,20 @@
 				$tempsymb = "1";
 			}
 			if($array[$i] == 1 and $array[$i+1]==1)
+			{
+				$countsyb++;
+			}
+			if($array[$i] == 0 and $array[$i+1]==0)
+			{
+				$countsyb++;
+			}
 			
-			return $output = strval(countsyb) + tempsymb;
+			$output = strval(countsyb) + tempsymb;
+			
 		}
+		return $output;
 	}
-	function decompresser
+	function decompresser()
 	{
 		
 		
